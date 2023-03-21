7d3f45b260c661f3cb69a187427ecaf007e05535
Faouizi MZEBLA <faouizi.mzebla@ynov.com>
2023-03-21 12:13:09 +0100

    Update Home

--- RecettesRamadan/src/screens/home.js
+++ RecettesRamadan/src/screens/home.js
@@ -1,18 +1,6 @@
 import HomeScreen from '../components/HomeScreen';
-import styled from 'styled-components/native';
-import Footer from '../components/footer';
 
 const Home = () => {
-  return (
-    <Container>
-      <HomeScreen />
-    </Container>
-  );
+  return <HomeScreen />;
 };
 export default Home;
-
-const Container = styled.View`
-  flex: 1;
-  background-color: white;
-  padding: 16px;
-`;
